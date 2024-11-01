95087278791feb0ec319a6d49053968fe4a5ba90
devgeekm <118623500+devgeekm@users.noreply.github.com>
2024-10-31 22:14:17 -0500

    modified run

--- run.py
+++ run.py
@@ -1,9 +1,7 @@
 from fastapi import FastAPI
 from app.api.endpoints import transcribe
-import subprocess
 import os
-import signal
-import time
+import uvicorn
 
 app = FastAPI()
 
@@ -13,29 +11,8 @@ app.include_router(transcribe.router, prefix="/api/v1/transcribe", tags=["transc
 def read_root():
     return {"message": "Welcome to the FastAPI YouTube Transcription Service"}
 
-def clean_port(port):
-    try:
-        # Encuentra el proceso que está utilizando el puerto
-        result = subprocess.run(["lsof", "-i", f":{port}"], capture_output=True, text=True)
-        lines = result.stdout.splitlines()
-        if len(lines) > 1:
-            # Extrae el PID del proceso
-            pid = int(lines[1].split()[1])
-            # Mata el proceso
-            os.kill(pid, signal.SIGKILL)
-            print(f"Proceso {pid} que utilizaba el puerto {port} ha sido terminado.")
-        else:
-            print(f"No se encontró ningún proceso utilizando el puerto {port}.")
-    except Exception as e:
-        print(f"Error al limpiar el puerto {port}: {str(e)}")
-
-def is_port_in_use(port):
-    result = subprocess.run(["lsof", "-i", f":{port}"], capture_output=True, text=True)
-    lines = result.stdout.splitlines()
-    return len(lines) > 1
-
-import uvicorn
-
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 8000))
+    # Obtener el puerto desde la variable de entorno PORT (Azure asigna este puerto automáticamente)
+    port = int(os.getenv("PORT", 5000))
+    # Ejecutar la aplicación con Uvicorn
     uvicorn.run(app, host="0.0.0.0", port=port)
--- startup.sh
+++ startup.sh
@@ -4,4 +4,4 @@
 apt-get update && apt-get install -y ffmpeg
 
 # Iniciar la aplicación
-gunicorn --bind=0.0.0.0 --timeout 600 run:app
+gunicorn --bind=0.0.0.0 --timeout 600 run:run
