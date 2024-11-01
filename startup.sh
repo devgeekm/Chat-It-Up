2cb00c8ea110dc67fe7d811f5024dcb55a480292
devgeekm <118623500+devgeekm@users.noreply.github.com>
2024-10-31 22:26:22 -0500

    run:app

--- startup.sh
+++ startup.sh
@@ -3,5 +3,8 @@
 # Descargar e instalar FFmpeg
 apt-get update && apt-get install -y ffmpeg
 
+# Verificar la instalación de FFmpeg
+ffmpeg -version
+
 # Iniciar la aplicación
-gunicorn --bind=0.0.0.0 --timeout 600 run:run
+gunicorn --bind=0.0.0.0 --timeout 600 run:app
